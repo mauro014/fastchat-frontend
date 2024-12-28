e69990ed794fb1e5e4a30d41306f0fec1ae0948b
mauro014 <mauro014@gmail.com>
2024-12-28 10:53:46 +0200

    Another URL es fixed.

--- src/Services/api.js
+++ src/Services/api.js
@@ -5,7 +5,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 export const getAllMessages = async () => {
   try {
     console.log(API_URL);
-    const response = await axios.get(`http://${API_URL}/getAllMessages`);
+    const response = await axios.get(`${API_URL}/getAllMessages`);
     return response.data;
   } catch (error) {
     console.error('Error fetching previous messages:', error);
